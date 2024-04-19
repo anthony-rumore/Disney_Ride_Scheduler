06814fe5f31cf22aec303a531527c61cf5c9d89d
AJRUMORE <ajrumore1+github@gmail.com>
2024-04-19 14:45:48 -0400

    Added Ride.h and Ride.cpp

---
+++ Ride.cpp
@@ -0,0 +1,5 @@
+//
+// Created by AJ Rumore on 4/19/24.
+//
+
+#include "Ride.h"
---
+++ Ride.h
@@ -0,0 +1,20 @@
+//
+// Created by AJ Rumore on 4/19/24.
+//
+#include <string>
+
+
+#ifndef DSA_P3_RIDE_H
+#define DSA_P3_RIDE_H
+
+using namespace std;
+
+class Ride {
+private:
+    string name;
+    int popularityIndex;
+    int averageWaitTime[12][12];
+};
+
+
+#endif //DSA_P3_RIDE_H
